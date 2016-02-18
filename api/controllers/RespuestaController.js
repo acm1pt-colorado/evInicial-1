ab73aaf9eb6134aad5b58ec235ca9e33aa4c42ec
acm1pt-colorado <luisadanjimenez@gmail.com>
2016-02-18 12:29:11 +0100

    Creando policies para que el alumno cree la respuesta asociada

--- api/controllers/RespuestaController.js
+++ api/controllers/RespuestaController.js
@@ -12,10 +12,23 @@ module.exports = {
 				where: {opcion: Number(respuesta), nombre: "fraccion"}
 			}).then(function(subopcion){
 				var puntuacion = subopcion.valor;
-				Respuesta.create({valor: respuesta, puntuacion: puntuacion})
-				.exec(function createCB(err, created){
-					res.send('Created respuesta : Valor: ' + created.valor +" Puntuacion : "+ created.puntuacion);
+				var alumno = req.session;
+				Alumno.findOne({
+					where: {user: alumno.passport.id}
+				}).then(function(alumno){
+					if(alumno){
+						Respuesta.create({valor: respuesta, puntuacion: puntuacion, alumno: alumno.id})
+						.exec(function createCB(err, created){
+							res.send('Created respuesta : Valor: ' + created.valor +" Puntuacion : "+ created.puntuacion);
+						})
+					}else{
+						res.send("No estas autenticado como usuario alumno");
+					}
 				})
+
+
+				
+				
 			})
 	}
 };
